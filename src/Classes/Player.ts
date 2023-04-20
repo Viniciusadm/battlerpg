288138c8fbe157ce00f7607d8f0bdb827d989863
viniciusam <viniciusam.22@gmail.com>
2023-04-19 21:51:07 -0300

    Spells and energy

--- src/Classes/Player.ts
+++ src/Classes/Player.ts
@@ -1,5 +1,7 @@
 import Character from "./Abstracts/Character";
 import { d20, d6 } from "../Helpers/dices";
+import { Spell } from "../Database/spells";
+import Dice from "./Dice";
 
 export type PlayerSkills = {
     strength: number;
@@ -12,13 +14,16 @@ export type PlayerSkills = {
 
 export default class Player extends Character {
     private readonly skills: PlayerSkills;
+    private readonly spells: Spell[] = [];
     private readonly armor: number = 0;
+    private energy: number = 0;
 
     constructor(attributes: { name: string }, skills: PlayerSkills) {
         super(attributes);
         this.skills = skills;
-        this.attributes = { ...attributes, life: this.getInitialLife(), totalLife: this.getInitialLife() };
+        this.attributes = {...attributes, life: this.getInitialLife(), totalLife: this.getInitialLife()};
         this.armor = this.getCalculatedArmor();
+        this.energy = this.getCalculatedEnergy();
     }
 
     public getExpecifiedSkill(skill: keyof PlayerSkills): number {
@@ -58,5 +63,51 @@ export default class Player extends Character {
 
     private getCalculatedArmor(): number {
         return 10 + this.getExpecifiedSkill('dexterity');
+    }
+
+    private getCalculatedEnergy(): number {
+        return 20 + this.getExpecifiedSkill('intelligence') * 2;
+    }
+
+    public addSpell(spell: Spell): void {
+        this.spells.push(spell);
+    }
+
+    private decreaseEnergy(spell: Spell): void {
+        this.energy -= spell.energyCost;
+    }
+
+    public useSpell(spell: Spell, resists: number): number|boolean {
+        if (spell && this.energy >= spell.energyCost) {
+            this.decreaseEnergy(spell);
+
+            if (resists > this.getMyResist()) {
+                return false;
+            }
+
+            let total = 0;
+
+            for (const dice of spell.dices) {
+                const { value} = new Dice(dice).roll();
+                total += value;
+            }
+
+            return total + this.getExpecifiedSkill('intelligence');
+        }
+
+        return 0;
+    }
+
+    public canSpell(spellName: string): boolean {
+        const spell = this.getSpell(spellName);
+        return spell && this.energy >= spell.energyCost;
+    }
+
+    public getSpell(spellName: string): Spell | undefined {
+        return this.spells.find(spell => spell.name === spellName);
+    }
+
+    private getMyResist(): number {
+        return 8 + this.getExpecifiedSkill('intelligence');
     }
 }
